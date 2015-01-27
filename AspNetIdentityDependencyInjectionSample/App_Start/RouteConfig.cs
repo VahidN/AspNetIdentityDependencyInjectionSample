a2c6e02bf73fd074d31ee6c17722d6e17e910e6d
VahidN <vahid_nasiri@yahoo.com>
2015-01-27 13:37:20 +0330

    Added IgnoreRoute browserlink.

--- AspNetIdentityDependencyInjectionSample/App_Start/RouteConfig.cs
+++ AspNetIdentityDependencyInjectionSample/App_Start/RouteConfig.cs
@@ -8,6 +8,7 @@ namespace AspNetIdentityDependencyInjectionSample
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{*browserlink}", new { browserlink = @".*/arterySignalR/ping" });
 
             routes.MapRoute(
                 name: "Default",
