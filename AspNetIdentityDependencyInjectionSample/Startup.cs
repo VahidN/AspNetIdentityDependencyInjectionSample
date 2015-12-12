e0a78865737d30ba823a060a2068b3b0d0e3c836
VahidN <vahid_nasiri@yahoo.com>
2015-12-12 14:37:05 +0330

    Improved Startup.cs file.

--- AspNetIdentityDependencyInjectionSample/Startup.cs
+++ AspNetIdentityDependencyInjectionSample/Startup.cs
@@ -20,6 +20,8 @@ namespace AspNetIdentityDependencyInjectionSample
 
         private static void configureAuth(IAppBuilder app)
         {
+            app.CreatePerOwinContext(
+               () => SmObjectFactory.Container.GetInstance<IApplicationUserManager>());						
             SmObjectFactory.Container.Configure(config =>
             {
                 config.For<IDataProtectionProvider>()
@@ -53,9 +55,6 @@ namespace AspNetIdentityDependencyInjectionSample
             // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
             // This is similar to the RememberMe option when you log in.
             app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
-
-            app.CreatePerOwinContext(
-               () => SmObjectFactory.Container.GetInstance<IApplicationUserManager>());
 
             // Uncomment the following lines to enable logging in with third party login providers
             //app.UseMicrosoftAccountAuthentication(
